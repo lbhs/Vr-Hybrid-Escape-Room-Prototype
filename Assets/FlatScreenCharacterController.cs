8123c11bb8c08ea7cdc14ed196cf376c5a0b7307
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-15 14:19:37 -0700

    added gravity

--- Assets/FlatScreenCharacterController.cs
+++ Assets/FlatScreenCharacterController.cs
@@ -6,30 +6,46 @@ public class FlatScreenCharacterController : MonoBehaviour
 {
     public CharacterController CC;
     public float speed = 6f;
+    public float gravity = -9.81f;
     public Camera myCamera;
+    [Header("A child of the camera")] //used to calculate the position of the held gameobject without parenting the held gameobject
+    public GameObject fakeChild;
+
     private Transform CurrentSlectedObject;
     private Transform CurrentHeldObject;
-    //private Vector3 offset;
-    public GameObject fakeChild;
-    // Update is called once per frame
+    private Vector3 velocityStore; //velocity from the last frame
+
     void Update()
     {
+        ///----------------------------player movement-----------------   https://youtu.be/_QajrabyTJc?t=889
+         //doesn't accelerate if the player is on the ground
+        if (CC.isGrounded && velocityStore.y < 0)
+        {
+            velocityStore.y = -2; //makes sure the player stick to the ground just in case
+        }
         //gets wasd input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         //make a vector based on the rotation of the character
         Vector3 move = transform.right * x + transform.forward * z;
-
-        //To add gravity: https://youtu.be/_QajrabyTJc?t=889
+        
         //Actually moves player
         CC.Move(move * speed * Time.deltaTime);
 
-        //RayCast
+       
+
+        //Apply Gravity
+        velocityStore.y += gravity * Time.deltaTime;
+        CC.Move(velocityStore * Time.deltaTime);
+      
+
+        ///-------------------picking up objects-------------------------
+        //RayCast to detect what were looking at
         Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         int layer_mask = LayerMask.GetMask("CanInteract");
-        if (Physics.Raycast(ray, out hit, 5f, layer_mask))
+        if (Physics.Raycast(ray, out hit, 1f, layer_mask))
         {
             //draw invisible ray cast/vector
             Debug.DrawLine(ray.origin, hit.point);
@@ -40,6 +56,7 @@ public class FlatScreenCharacterController : MonoBehaviour
             CurrentSlectedObject = null;
         }
 
+        //Input detecting
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Grab();
@@ -49,69 +66,16 @@ public class FlatScreenCharacterController : MonoBehaviour
             UnGrab();
         }
 
+        //move the object if grabbed
         if(CurrentHeldObject != null)
         {
-        //    Vector3 target = fakeChild.transform.position;
-        //    CurrentHeldObject.GetComponent<Rigidbody>().velocity = target;
-            //RaycastHit hitInfo;
-            //if (Physics.Linecast(CurrentHeldObject.position, target, out hitInfo))
-            //{
-            //    CurrentHeldObject.GetComponent<Rigidbody>().MovePosition(hitInfo.point);
-            //    //movement code here , tell it to move to hitInfo.point
-            //}
-            //else
-            //{
-            //    CurrentHeldObject.GetComponent<Rigidbody>().MovePosition(target);
-            //    //tell it to move to target.position
-            //}
-
-            //Vector3 diff = CurrentHeldObject.position - fakeChild.transform.position;
-            //diff.Normalize();
-
-            //Rigidbody rb = CurrentHeldObject.GetComponent<Rigidbody>();
-            ////rb.AddForce(-diff*30);
-            ////CurrentHeldObject.GetComponent<Rigidbody>().MovePosition(transform.position - diff);
-            CurrentHeldObject.GetComponent<Rigidbody>().MovePosition( fakeChild.transform.position);
-            CurrentHeldObject.GetComponent<Rigidbody>().MoveRotation(fakeChild.transform.rotation);
-            //CurrentHeldObject.transform.position = myCamera.transform.TransformPoint(transform.localPosition);
-        }
-        //RayCast to grab objects
-        /*if (Input.GetKey(KeyCode.Mouse0))
-        {
-            Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            int layer_mask = LayerMask.GetMask("CanInteract");
-            if (Physics.Raycast(ray, out hit, 5f, layer_mask))
+            //OVRGrabbable is the vr script, but I'm using it here to detect weather is a clickable object or a grabbable object
+            if (CurrentHeldObject.GetComponent<OVRGrabbable>() != null)
             {
-                //draw invisible ray cast/vector
-                Debug.DrawLine(ray.origin, hit.point);
-                CurrentSlectedObject = hit.transform;
+                CurrentHeldObject.GetComponent<Rigidbody>().MovePosition(fakeChild.transform.position);
+                CurrentHeldObject.GetComponent<Rigidbody>().MoveRotation(fakeChild.transform.rotation);
             }
         }
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            if(CurrentSlectedObject != null)
-            {
-                if (CurrentSlectedObject.GetComponent<OVRGrabbable>() != null)
-                {
-                    CurrentHeldObject = CurrentSlectedObject;
-                    offset = myCamera.transform.position - CurrentHeldObject.position;
-                }
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            CurrentHeldObject.parent = null;
-            CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = false;
-            CurrentHeldObject = null;
-            CurrentSlectedObject = null;
-        }
-
-        if(CurrentHeldObject != null)
-        {
-            CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = true;
-            CurrentHeldObject.parent = myCamera.transform;
-        }*/
     }
 
     void Grab()
@@ -120,18 +84,17 @@ public class FlatScreenCharacterController : MonoBehaviour
         {
             if (CurrentSlectedObject.GetComponent<OVRGrabbable>() != null)
             {
-
                 fakeChild.transform.position = CurrentSlectedObject.transform.position;
                 fakeChild.transform.rotation = CurrentSlectedObject.transform.rotation;
                 CurrentHeldObject = CurrentSlectedObject;
-                CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = true;
-                //offset = CurrentHeldObject.position - myCamera.transform.position;
+                CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = true; //disable physic so it doesn't fight with this script
             }
         }
     }
+
     void UnGrab()
     {
-        CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = false;
+        CurrentHeldObject.GetComponent<Rigidbody>().isKinematic = false; //re-enabled physics
         CurrentHeldObject = null;
     }
 
