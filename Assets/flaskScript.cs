58efc92ea31a69d96da77ff91212ffc773ea75fb
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-16 16:19:52 -0700

    added a flask

---
+++ Assets/flaskScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flaskScript : MonoBehaviour
+{
+    public Transform EmitPoint;
+    public GameObject particle;
+    public int pourThreshold = 45;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CaculatePourAngles() <= pourThreshold)
+        {
+            Instantiate(particle, EmitPoint.position, EmitPoint.rotation);
+        }
+        else
+        {
+            
+        }
+    }
+
+    float CaculatePourAngles()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+}
