fa6268b95bdc27a5104c74e6b8214433edeeb8ed
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-16 14:30:19 -0700

    added flask

--- Assets/PlatformManager.cs
+++ Assets/PlatformManager.cs
@@ -6,8 +6,13 @@ public class PlatformManager : MonoBehaviour
 {
     //determines what character controller to spawn
     public static bool isVR = false;
+    //Different Controllers for different platforms
     public GameObject VRController;
     public GameObject FlatScreenController;
+    //Event systems are how unity transfers mouse or vr input into things like buttons
+    public GameObject FlatEventSystem;
+    public GameObject VREventSystem;
+    //universal reference to the player;
     public static GameObject player;
 
 
