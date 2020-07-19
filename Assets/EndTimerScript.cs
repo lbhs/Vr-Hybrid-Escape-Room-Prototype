669caa3dc24d2f67c77a2aa21bf82965544ef689
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-18 18:33:29 -0700

    VR works! now to find out what happned to flat screen support!

--- Assets/EndTimerScript.cs
+++ Assets/EndTimerScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EndTimerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.tag == "Player")
+        {
+            PlatformManager.CountingDown = false;
+        }
     }
 }
