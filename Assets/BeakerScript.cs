06ada6b4868fb4de973dd4c8105a30f29fdd7b97
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-22 15:12:32 -0700

    added beaker

--- Assets/BeakerScript.cs
+++ Assets/BeakerScript.cs
@@ -7,10 +7,14 @@ public class BeakerScript : MonoBehaviour
     [Range(0, 1)]
     public float waterPercent;
     public Transform Liquid;
-    public float fullValue;
-    public float emptyValue;
-    public float fillRate;
-    public float drainRate;
+    public float fullValue=0.85f;
+    public float emptyValue=0.03f;
+    public float fillRate=0.001f;
+    public float drainRate=0.001f;
+
+    public Transform EmitPoint;
+    public GameObject particle;
+    public int pourThreshold = 45;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +24,46 @@ public class BeakerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (waterPercent < 0)
+        {
+            waterPercent = 0;
+        }
+        else if(waterPercent >1)
+        {
+            waterPercent = 1;
+        }
         //calculate the water level
         float val = ((fullValue - emptyValue) * waterPercent) + emptyValue;
         //apply it
         Liquid.localScale = new Vector3(Liquid.localScale.x, Liquid.localScale.y, val);
+
+        //spilling
+        if (CaculatePourAngles() <= pourThreshold)
+        {
+            if (waterPercent > 0 && waterPercent <= 1)
+            {
+                Instantiate(particle, EmitPoint.position, EmitPoint.rotation);
+                waterPercent = waterPercent - fillRate;
+            }
+        }
+        else
+        {
+
+        }
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        if (waterPercent <= 1 && waterPercent >= 0)
+        if (waterPercent <= 1 && waterPercent >= 0 )
         {
             waterPercent = waterPercent + fillRate;
+
+            //----------------------------------------------to-do: change color
         }
+    }
+
+    float CaculatePourAngles()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
     }
 }
