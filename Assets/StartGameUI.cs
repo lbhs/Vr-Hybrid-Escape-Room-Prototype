8ca673470fccb37855684429d8773f5b6d1d33eb
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-23 16:23:01 -0700

    Added better start Scene

---
+++ Assets/StartGameUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameUI : MonoBehaviour
+{ public int VRScene=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlatformManager.isVR == true)
+        {
+            SceneManager.LoadScene(VRScene);
+        }
+    }
+}
